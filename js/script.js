535d47ebdd934ab18129fa39a5b1d46542285214
whl <wplovitz@gmail.com>
2022-08-22 18:33:10 -0700

    changed question block

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ var startButton = document.getElementsByClassName('startb');
 var enterButton = document.getElementsByClassName('enterb');
 var shuffledQuestions , currentQestion; 
 var questionEl = document.getElementById("question");
-var aButtonEl1 = document.getElementById('op1');
+/* var aButtonEl1 = document.getElementById('op1');
 var aButtonEl2 = document.getElementById("op2");
 var aButtonEl3 = document.getElementById('op3');
-var aButtonEl4 = document.getElementById("op4");
+var aButtonEl4 = document.getElementById("op4"); */
 var optionsEl = document.getElementsByClassName("option")
 var timer = document.getElementById('timer-count');
 var result = document.getElementsByClassName('result');
@@ -14,82 +14,65 @@ var score = 0;
 var Questions = [{ 
     id: 0,
     q: "0",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "2: ", isCorrect: false},
-        { text: "3: ", isCorrect: false},
-        { text: "4: ", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+        
 },{ id: 1,
     q: "1",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "awnser", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+
 },{ id: 2,
     q: "2",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "answer", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"    
+
 },{ id: 3,
     q: "3",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "answer", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"    
+
 },{ id: 4,
     q: "4",
-    a: [{ text: "a. ", isCorrect: true },
-        { text: "wrong", isCorrect: false },
-        { text: "wrong", isCorrect: false },
-        { text: "wrong", isCorrect: false }],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+
 },{ id: 5,
     q: "5",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+
 },{ id: 6,
     q: "6",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+
 },{ id: 7,
     q: "7",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"
+
 },{ id: 8,
     q: "8",
-    a: [{ text: "a. ", isCorrect: true},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false},
-        { text: "wrong", isCorrect: false}],
-        answers: "a"
+    a: ["a. ", "b.", "c. ", "d. "],
+    answers: "a"    
 }   
 ]
 function checker(event) {
     event.preventDefault();
 
     
-    var answers;
-    answers = Questions[questionNumber].a[0].isCorrect;
+    
+    
 
-    if (Questions[questionNumber].answers !== true) {
+    if (Questions[questionNumber].answers == event.target.value) {
         result.textContent = 'Correct!';
         score = score++;
+        console.log(score)
     } else {
         seconds = seconds - 5;
-        result.textContent = "Nope, That is incorect" + Questions[questionNumber].answers + ",";
+        result.textContent = "Nope, That is incorect" + Questions[questionNumber].a + ",";
+        console.log(score)
     }
 
     if (questionNumber < Questions.length -1 ) {
@@ -150,16 +133,16 @@ function showQuestion(id) {
     var op3 = document.getElementById('op3');
     var op4 = document.getElementById('op4');
 
-    op1.innerText = Questions[id].a[0].text;
-    op2.innerText = Questions[id].a[1].text;
-    op3.innerText = Questions[id].a[2].text;
-    op4.innerText = Questions[id].a[3].text;
+    op1.innerText = Questions[id].a[0];
+    op2.innerText = Questions[id].a[1];
+    op3.innerText = Questions[id].a[2];
+    op4.innerText = Questions[id].a[3];
     
 
-    op1.value = Questions[id].a[0].isCorrect;
-    op2.value = Questions[id].a[1].isCorrect;
-    op3.value = Questions[id].a[2].isCorrect;
-    op4.value = Questions[id].a[3].isCorrect;
+    op1.value = Questions[id].a[0];
+    op2.value = Questions[id].a[1];
+    op3.value = Questions[id].a[2];
+    op4.value = Questions[id].a[3];
 
     var selected = " ";
 
@@ -192,7 +175,7 @@ function showQuestion(id) {
         selected = op4.value;
     })
 
-    enterButton[0].addEventListener('click', function() {
+    /* enterButton[0].addEventListener('click', function() {
         if (selected == 'ture') {
             result[0].innerHTML = 'true';
             result[0].style.color = 'green';
@@ -200,7 +183,7 @@ function showQuestion(id) {
             result[0].innerHTML = 'false';
             result[0].style.color = 'red';
         }
-    })
+    }) */
  }
 
  enterButton[0].addEventListener("click", checker)
