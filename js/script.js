3dd5766ae798661bf614524ebdf50e5661b2fcd9
whl <wplovitz@gmail.com>
2022-08-23 19:56:18 -0700

    trying to fix the code so checker function works propperly

--- js/script.js
+++ js/script.js
@@ -1,66 +1,70 @@
 var startButton = document.getElementsByClassName('startb');
 var enterButton = document.getElementsByClassName('enterb');
-var shuffledQuestions , currentQestion; 
+
 var questionEl = document.getElementById("question");
 var op1 = document.getElementById('op1');
 var op2 = document.getElementById("op2");
 var op3 = document.getElementById('op3');
 var op4 = document.getElementById("op4"); 
-var optionsEl = document.getElementsByClassName("option")
+//var optionsEl = document.getElementsByClassName("option");
 var timer = document.getElementById('timer-count');
-var result = document.getElementsByClassName('result');
+var result = document.getElementById('result');
 var score = 0;
 
 var Questions = [{ 
     id: 0,
     q: "0",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a. "
         
 },{ id: 1,
     q: "1",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a. "
 
 },{ id: 2,
     q: "2",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"    
+    answers: "a. "    
 
 },{ id: 3,
     q: "3",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"    
+    answers: "a. "    
 
 },{ id: 4,
     q: "4",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a. "
 
 },{ id: 5,
     q: "5",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a."
 
 },{ id: 6,
     q: "6",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a. "
 
 },{ id: 7,
     q: "7",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"
+    answers: "a."
 
 },{ id: 8,
     q: "8",
     a: ["a. ", "b.", "c. ", "d. "],
-    answers: "a"    
+    answers: "a. "    
 }   
 ]
 function checker(event) {
     event.preventDefault();
 
+        result.style.display = "block" ;
+        setTimeout(function() {
+            result.style.display = "none";
+        }, 1000)
 
     if (Questions[questionNumber].answers == event.target.value) {
         result.textContent = 'Correct!';
@@ -68,7 +72,7 @@ function checker(event) {
         console.log(score)
     } else {
         seconds = seconds - 5;
-        result.textContent = "Nope, That is incorect" + Questions[questionNumber].a + ",";
+        result.textContent = "Nope, That is incorect" + Questions[questionNumber].answers + ",";
         console.log(score)
     }
 
@@ -91,14 +95,15 @@ function gameOver() {
     timer.style.display = 'none';
 }
 
-
-startButton[0].addEventListener("click", function startGame() {
-    console.log('click')
-    startButton?.[0]?.classList?.add('hide')
+function startGame() {
     questionNumber = 0
     downer();
     showQuestion(questionNumber);
-})
+}
+
+
+
+
 
 var seconds = 60;
 
@@ -120,10 +125,10 @@ function showQuestion(id) {
     
     questionEl.innerText = Questions[id].q;
    
-    op1.innerText = Questions[id].a[0];
-    op2.innerText = Questions[id].a[1];
-    op3.innerText = Questions[id].a[2];
-    op4.innerText = Questions[id].a[3];
+    op1.textContent = Questions[id].a[0];
+    op2.textContent = Questions[id].a[1];
+    op3.textContent = Questions[id].a[2];
+    op4.textContent = Questions[id].a[3];
 
     op1.addEventListener('click', () => {
         op1.style.color = 'green';
@@ -153,9 +158,10 @@ function showQuestion(id) {
         op4.style.color = 'green';
         
     })
+
+
 }
- 
 
- enterButton[0].addEventListener("click", checker)
+startButton.addEventListener("click", startGame);
 
-    
+enterButton[0].addEventListener('click', checker)
